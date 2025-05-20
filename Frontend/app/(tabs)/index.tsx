c12ab30d75f40d65877a9d5def685b0edd4aba08
pnQuanq <phamquang13103@gmail.com>
2025-05-20 14:49:14 +0700

    create FlashSale section for Home page

--- Frontend/app/(tabs)/index.tsx
+++ Frontend/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, ActivityIndicator, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { CategoryType, ProductType } from "@/types/type";
@@ -9,17 +9,20 @@ import ProductItem from "@/components/ProductItem";
 import { Colors } from "@/constants/Colors";
 import ProductList from "@/components/ProductList";
 import Categories from "@/components/Categories";
+import FlashSale from "@/components/FlashSale";
 import { Personal_IP } from "@/constants/ip";
 
 type Props = {};
 
 const HomeScreen = (props: Props) => {
   const [products, setProducts] = useState<ProductType[]>([]);
+  const [saleProducts, setSaleProducts] = useState<ProductType[]>([]);
   const [categories, setCategories] = useState<CategoryType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   useEffect(() => {
     getProducts();
     getCategories();
+    getSaleProducts();
   }, []);
 
   const getProducts = async () => {
@@ -48,10 +51,31 @@ const HomeScreen = (props: Props) => {
     console.log("categories", response.data.data);
   };
 
+  const getSaleProducts = async () => {
+      const URL = "http://" + Personal_IP.data + ":3000/product/get-all-products";
+      const response = await axios.get(URL).catch((error) => {
+        console.log(
+          "There has been a problem with your fetch operation: " + error.message
+        );
+        throw error;
+      });
+      setSaleProducts(response.data.data);
+      setIsLoading(false);
+    };
+
+    if(isLoading) {
+        return(
+            <View>
+            <ActivityIndicator size={'large'}/>
+            </View>
+        )
+    }
+
   return (
     <>
       <Stack.Screen options={{ headerShown: true, header: () => <Header /> }} />
       <Categories categories={categories}></Categories>
+      <FlashSale products={saleProducts}/>
       <ProductList products={products} />
     </>
   );
