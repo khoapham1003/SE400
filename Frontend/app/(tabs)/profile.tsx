39a9b1823f2a2522bcd724309b6d337a6c9a568f
Khoa Phạm <115645999+khoapham1003@users.noreply.github.com>
2025-05-23 12:29:34 +0700

    FE: Create Profile Screen

--- Frontend/app/(tabs)/profile.tsx
+++ Frontend/app/(tabs)/profile.tsx
@@ -1,22 +1,106 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+import { useHeaderHeight } from "@react-navigation/elements";
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
-type Props = {}
+type Props = {};
 
 const ProfileScreen = (props: Props) => {
-  return (
-    <View style={styles.container}>
-      <Text>Profile Screen</Text>
-    </View>
-  )
-}
+  const headerHeight = useHeaderHeight();
 
-export default ProfileScreen
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: true, headerTransparent: true }} />
+      <View style={[styles.container, { marginTop: headerHeight }]}>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={{
+              uri: "https://xsgames.co/randomusers/avatar.php?g=pixel&key=1",
+            }}
+          />
+          <Text style={styles.userName}>PKBinnnn Name</Text>
+        </View>
+        <View style={styles.buttonWrapper}>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name="person-outline" size={20} color={Colors.black} />
+            <Text style={styles.buttonText}>Your Order</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name="heart-outline" size={20} color={Colors.black} />
+            <Text style={styles.buttonText}>Your Wishlist</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name="card-outline" size={20} color={Colors.black} />
+            <Text style={styles.buttonText}>Payment History</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name="gift-outline" size={20} color={Colors.black} />
+            <Text style={styles.buttonText}>Reward Point</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons
+              name="help-circle-outline"
+              size={20}
+              color={Colors.black}
+            />
+            <Text style={styles.buttonText}>Customer Support</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name="pencil-outline" size={20} color={Colors.black} />
+            <Text style={styles.buttonText}>Edit Profile</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name="settings-outline" size={20} color={Colors.black} />
+            <Text style={styles.buttonText}>Settings</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Ionicons name="log-out-outline" size={20} color={Colors.black} />
+            <Text style={styles.buttonText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default ProfileScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    padding: 20,
+  },
+  userName: {
+    fontSize: 20,
+    fontWeight: "500",
+    color: Colors.black,
+    marginTop: 10,
+  },
+  buttonWrapper: {
+    marginTop: 20,
+    gap: 10,
+  },
+  button: {
+    padding: 10,
+    borderTopColor: Colors.lightGray,
+    borderWidth: 1,
+    borderRadius: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: "400",
+    color: Colors.black,
+  },
+});
