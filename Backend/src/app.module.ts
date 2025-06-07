dd811d4c371fd3b5824eb6b0892ec30392fc55c2
Khoa Phạm <115645999+khoapham1003@users.noreply.github.com>
2025-06-08 00:52:06 +0700

    bE: fix Role Control

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -25,6 +25,12 @@ import { OrderitemModule } from './modules/orderitem/orderitem.module';
 import { CartitemModule } from './modules/cartitem/cartitem.module';
  import { AuthModule } from './modules/auth/auth.module';
 import { AuthMiddleware } from './Common/middlewares/auth.middleware';
+import { CartController } from './modules/cart/controller/cart.controller';
+import { OrderController } from './modules/order/controller/order.controller';
+import { TransactionController } from './modules/transaction/controller/transaction.controller';
+import { UserController } from './modules/user/controller/user.controller';
+import { OrderItemController } from './modules/orderitem/controller/orderitem.controller';
+import { CartitemController } from './modules/cartitem/controller/cartitem.controller';
 
 @Module({
   imports: [
@@ -60,6 +66,33 @@ import { AuthMiddleware } from './Common/middlewares/auth.middleware';
 })
 export class AppModule { configure(consumer: MiddlewareConsumer) {
   consumer
-    .apply(AuthMiddleware)
-    .forRoutes('orders', 'cart', 'cartitem', 'order-item', 'user'); 
+      .apply(AuthMiddleware)
+      .forRoutes(
+        {path: 'color/create-color', method: RequestMethod.POST },
+        {path: 'color/update-corlor/:id', method: RequestMethod.PATCH },
+        {path: 'color/delete-color/:id', method: RequestMethod.DELETE },
+        {path: '/sizes/create-size', method: RequestMethod.POST},
+        {path: '/sizes/update-size/:id', method: RequestMethod.PATCH},
+        {path: '/sizes/delete-size/:id', method: RequestMethod.DELETE},
+        {path: '/category/create-category', method: RequestMethod.POST},
+        {path: '/category/update-category/:id', method: RequestMethod.PATCH},
+        {path: '/category/delete-category/:id', method: RequestMethod.DELETE},
+        {path: '/product-variants/create-productvariant', method: RequestMethod.POST},
+        {path: '/product-variants/update-productvariant/:id', method: RequestMethod.PATCH},
+        {path: '/product-variants/delete-productvariant/:id', method: RequestMethod.DELETE},
+        {path: '/category-product/create', method: RequestMethod.POST},
+        {path: '/category-product/update/:id', method: RequestMethod.PATCH},
+        {path: '/category-product/delete/:id', method: RequestMethod.DELETE},
+        {path: '/category-product/all', method: RequestMethod.GET},
+        {path: '/product/create-product', method: RequestMethod.POST},
+        {path: '/product/update-product/:id', method: RequestMethod.PATCH},
+        {path: '/product/delete-product/:id', method: RequestMethod.DELETE},
+        CartController,
+        OrderController,
+        CartitemController,
+        OrderItemController,
+        UserController,
+        TransactionController,
+      );
+      // .forRoutes('orders', 'cart', 'cartitem', 'order-item', 'user', 'color');
 }}
